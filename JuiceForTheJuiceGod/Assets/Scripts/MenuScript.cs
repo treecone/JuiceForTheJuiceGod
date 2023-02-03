b82e24e1d6e0e6fce33737ed89c9db846db31dbe
bubbykinz <70727087+bubbykinz@users.noreply.github.com>
2023-02-02 18:22:25 -0800

    Hayden Camera, Map, and Menu
    Updated camera viewpoint, added more placeholder tiles for the gamescene, created menu and creditsscene and first pass of menu script

---
+++ JuiceForTheJuiceGod/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
